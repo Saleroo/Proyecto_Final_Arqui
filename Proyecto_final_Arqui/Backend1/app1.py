7849ff1d9d0086507ec8c08d86170a81f961d789
Saleroo <50508769+Saleroo@users.noreply.github.com>
2021-12-26 21:06:13 -0300

    Add files via upload

--- Proyecto_final_Arqui/Backend1/app1.py
+++ Proyecto_final_Arqui/Backend1/app1.py
@@ -1,18 +1,26 @@
+
+import requests
+import os
 from flask import Flask
 
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def Dolar():
+    env = os.environ.get('other_pod')
+    env = str(env)
+    r= requests.get(env)
+
+    data = r.text
+    print(data)
+    return data
+
+app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 4444)),debug=True)
 
 
 
 
-app= Flask(__name__)
 
-@app.route('/dolar')    
-def getDolar():
-    print("¿Backend2 me podrias decir cual es el valor del dolar actual?")
-    print("\n")
-    #ObtenerDolar()
-    return "<p> Succes </p>"
-
-app.run(host='0.0.0.0',port=8080,debug=True)
 
