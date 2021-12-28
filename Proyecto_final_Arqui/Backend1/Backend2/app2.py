fbdcdf984030e8580cba87e30cfe58f2e29e994c
Diertotech <87912147+Diertotech@users.noreply.github.com>
2021-12-27 21:20:31 -0300

    Add files via upload

--- Proyecto_final_Arqui/Backend1/Backend2/app2.py
+++ Proyecto_final_Arqui/Backend1/Backend2/app2.py
@@ -1,9 +1,27 @@
 import http.client
 from flask import Flask
+# or configure to use ELASTIC_APM in your application's settings
+from elasticapm.contrib.flask import ElasticAPM
 
+app = Flask(__name__)
+app.config['ELASTIC_APM'] = {
+# Set the required service name. Allowed characters:
+# a-z, A-Z, 0-9, -, _, and space
+'SERVICE_NAME': 'App2',
 
-app= Flask(__name__)
+# Use if APM Server requires a secret token
+'SECRET_TOKEN': 'IRY8JxQCLhC9BYyh0G',
 
+# Set the custom APM Server URL (default: http://localhost:8200)
+'SERVER_URL': 'https://7293afe83bfb4660bd8419d7428e13b4.apm.eu-west-1.aws.cloud.es.io:443',
+
+# Set the service environment
+'ENVIRONMENT': 'production',
+
+'DEBUG': True
+}
+
+apm = ElasticAPM(app)
 
 @app.route('/')
 def ObtenerDolar():
